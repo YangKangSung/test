111b1904baf0676b8f9e6b681984ada12f2302a2
YangKangSung <cyclestel@gmail.com>
2025-04-07 21:16:01 +0900

    test

--- anythingllm/run-anythingllm.sh
+++ anythingllm/run-anythingllm.sh
@@ -1,8 +1,9 @@
-export STORAGE_LOCATION=$HOME/anythingllm \
-	mkdir -p $STORAGE_LOCATION \
-	touch "$STORAGE_LOCATION/.env"
+export STORAGE_LOCATION=$HOME/anythingllm
+mkdir -p $STORAGE_LOCATION
+touch "$STORAGE_LOCATION/.env"
 
 docker run --restart always -p 3001:3001 \
+	--add-host=host.docker.internal:192.168.0.2 --net=host \
 	--cap-add SYS_ADMIN \
 	-v ${STORAGE_LOCATION}:/app/server/storage \
 	-v ${STORAGE_LOCATION}/.env:/app/server/.env \
